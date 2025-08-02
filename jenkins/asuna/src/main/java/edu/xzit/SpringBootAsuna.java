22bc931518531d189ae3e863dbd6c69b72659744
illyasivel1314 <illyasivel@aliyun.com>
2025-08-02 16:15:59 +0800

    init

---
+++ jenkins/asuna/src/main/java/edu/xzit/SpringBootAsuna.java
@@ -0,0 +1,12 @@
+package edu.xzit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootAsuna {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAsuna.class, args);
+    }
+}
---
+++ jenkins/illya/src/main/java/edu/xzit/SpringBootIllya.java
@@ -0,0 +1,11 @@
+package edu.xzit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIllya {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIllya.class, args);
+    }
+}
